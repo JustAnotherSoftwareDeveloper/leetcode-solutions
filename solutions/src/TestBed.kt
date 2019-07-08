e153ce10c969b40c1f578d2d04ea2b92126c874c
Michael Sila <michael.sila.jobs@gmail.com>
2019-07-08 08:48:38 -0600

    Word Search II
    This one was frustrating. I was make the trie trees on the possible
    paths instead of the words

--- solutions/src/TestBed.kt
+++ solutions/src/TestBed.kt
@@ -6,11 +6,21 @@ import kotlin.system.measureTimeMillis
 
 fun  main(args: Array<String>) {
 
-        val tester = NonOverlappingIntervals()
+        val tester = WordSearchTwo()
+        //val board = arrayOf(charArrayOf('o','a','a','n'), charArrayOf('e','t','a','e'), charArrayOf('i','h','k','r'), charArrayOf('i','f','l','v'))
+        //val board = arrayOf(charArrayOf('b','b','a','a','b','a'), charArrayOf('b','b','a','b','a','a'), charArrayOf('b','b','b','b','b','b'), charArrayOf('a','a','a','b','a','a'), charArrayOf('a','b','a','a','b','b'))
+        //val board = arrayOf(charArrayOf('a','b'), charArrayOf('a','a'))
+        val board = arrayOf(charArrayOf('b','a','a','b','a','b'),charArrayOf('a','b','a','a','a','a'),charArrayOf('a','b','a','a','a','b'),charArrayOf('a','b','a','b','b','a'),charArrayOf('a','a','b','b','a','b'),charArrayOf('a','a','b','b','b','a'),charArrayOf('a','a','b','a','a','b'))
+        //val board = arrayOf(charArrayOf('a'))
 
-        tester.eraseOverlapIntervals(arrayOf(intArrayOf(1,2), intArrayOf(2,3), intArrayOf(3,4), intArrayOf(1,3)))
-        tester.eraseOverlapIntervals(arrayOf(intArrayOf(1,2), intArrayOf(1,2), intArrayOf(1,2)))
-        tester.eraseOverlapIntervals(arrayOf(intArrayOf(1,2), intArrayOf(2,3)))
+        //val words = arrayOf("oath","pea","eat","rain")
+        //val words = arrayOf("ab","ac","abb","acb")
+        //val words = arrayOf("abbbababaa")
+        //val words = arrayOf("aba","baa","bab","aaab","aaaa","aaba","aaa")
+        val words = arrayOf("bbaabaabaaaaabaababaaaaababb","aabbaaabaaabaabaaaaaabbaaaba","babaababbbbbbbaabaababaabaaa","bbbaaabaabbaaababababbbbbaaa","babbabbbbaabbabaaaaaabbbaaab","bbbababbbbbbbababbabbbbbabaa","babababbababaabbbbabbbbabbba","abbbbbbaabaaabaaababaabbabba","aabaabababbbbbbababbbababbaa","aabbbbabbaababaaaabababbaaba","ababaababaaabbabbaabbaabbaba","abaabbbaaaaababbbaaaaabbbaab","aabbabaabaabbabababaaabbbaab","baaabaaaabbabaaabaabababaaaa","aaabbabaaaababbabbaabbaabbaa","aaabaaaaabaabbabaabbbbaabaaa","abbaabbaaaabbaababababbaabbb","baabaababbbbaaaabaaabbababbb","aabaababbaababbaaabaabababab","abbaaabbaabaabaabbbbaabbbbbb","aaababaabbaaabbbaaabbabbabab","bbababbbabbbbabbbbabbbbbabaa","abbbaabbbaaababbbababbababba","bbbbbbbabbbababbabaabababaab","aaaababaabbbbabaaaaabaaaaabb","bbaaabbbbabbaaabbaabbabbaaba","aabaabbbbaabaabbabaabababaaa","abbababbbaababaabbababababbb","aabbbabbaaaababbbbabbababbbb","babbbaabababbbbbbbbbaabbabaa")
+        //val words = arrayOf("a")
+        val solution = tester.findWords(board,words)
 
+        println(solution)
 
     }
---
+++ solutions/src/WordSearchTwo.kt
@@ -0,0 +1,57 @@
+
+
+class WordSearchTwo {
+
+    private val grid: MutableMap<Pair<Int, Int>, Char> = mutableMapOf()
+    private val foundWords = mutableSetOf<String>()
+    private class CharTree(val value: Char, val children: MutableMap<Char,CharTree?> = mutableMapOf() )
+    fun findWords(board: Array<CharArray>, words: Array<String>): List<String> {
+        grid.clear()
+        foundWords.clear()
+        for (i in 0 until board.size) { // m*n
+            for (j in 0 until board[i].size) {
+                grid[Pair(i, j)] = board[i][j]
+            }
+        }
+        val rootTree = CharTree('#')
+        words.forEach { buildTree(it,rootTree) }
+        grid.keys.forEach { traverseTree(it, mutableSetOf(), StringBuilder(), rootTree) }
+
+        return words.filter { foundWords.contains(it) }
+    }
+    private fun buildTree(word: String, tree: CharTree) {
+        val wordList = word.toMutableList()
+        var current = tree
+        while (wordList.isNotEmpty()) {
+            val currentChar = wordList.removeAt(0)
+            var child = current.children[currentChar]
+            if (child== null) {
+                child = CharTree(currentChar)
+                current.children[currentChar] = child
+            }
+            current = child
+        }
+    }
+    private fun traverseTree(location: Pair<Int,Int>, found: MutableSet<Pair<Int,Int>>, sb: StringBuilder, tree: CharTree?) {
+        if (tree == null) {
+            return
+        }
+        if (tree.value == '#') {
+            traverseTree(location,found,sb, tree.children[grid.getValue(location)])
+            return
+        }
+        found.add(location)
+        sb.append(tree.value)
+        foundWords.add(sb.toString())
+        listOf(
+                Pair(location.first + 1, location.second),
+                Pair(location.first - 1, location.second),
+                Pair(location.first, location.second + 1),
+                Pair(location.first, location.second - 1)
+        )
+                .filter { grid.containsKey(it) && !found.contains(it)}
+                .forEach{
+                    traverseTree(it, found.toMutableSet(), StringBuilder(sb), tree.children[grid.getValue(it)])
+                }
+    }
+}
